f45c31b3170cb243be9ec87a4db2aeeb2cc4e795
yezixigua <541889983@qq.com>
2020-01-10 16:34:32 +0800

    map

---
+++ BaiduMapTest/app/src/main/java/com/zz/baidumaptest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.zz.baidumaptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+  *
+  * @package:        com.zz.baidumaptest
+  * @className:      MainActivity
+  * @description:     java类作用描述
+  * @author:         liuzhuang
+  * @createDate:     2020/1/10 0010
+  * @updateUser:     更新者：
+  * @updateDate:     2020/1/10 0010
+  * @updateRemark:   更新说明：
+  * @version:        1.0
+ */
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private BMapManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
