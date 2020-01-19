bdbae5008f7a778341d9e98377b7983a4d8dcce0
yezixigua <541889983@qq.com>
2020-01-19 15:13:58 +0800

    add flask server

---
+++ flask/app.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from flask import request, jsonify, send_from_directory, abort
+import os
+import json
+import random
+
+app = Flask(__name__)
+
+video_file_name = ['video1.mp4', 'video2.mp4']
+pic_file_name = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg']
+
+xml = """
+<apps>
+<app>
+<id>1</id>
+<name>Google Maps</name>
+<version>1.0</version>
+</app>
+<app>
+<id>2</id>
+<name>Chrome</name>
+<version>2.1</version>
+</app>
+<app>
+<id>3</id>
+<name>Google Play</name>
+<version>2.3</version>
+</app>
+</apps>"""
+
+a_json = """
+[{"id":"5","version":"5.5","name":"Angry Birds"},
+{"id":"6","version":"7.0","name":"Clash of Clans"},
+{"id":"7","version":"3.5","name":"Hey Day"}]"""
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+    return xml
+
+
+@app.route('/get_xml')
+def get_xml():
+    return xml
+
+
+@app.route('/get_json')
+def get_json():
+    return a_json
+
+
+@app.route('/pic')
+def request_pic():
+
+    return_dict = {}
+
+    if request.method == "GET":
+
+        index = random.randint(0, len(pic_file_name) - 1)
+
+        return_dict['type'] = 'pic'
+        return_dict['index'] = index
+
+        if os.path.isfile(os.path.join('pic', pic_file_name[index])):
+            return json.dumps(return_dict)
+        abort(404)
+
+
+@app.route('/video')
+def download_video():
+
+    return_dict = {}
+
+    index = random.randint(0, len(video_file_name) - 1)
+
+    return_dict['type'] = 'video'
+    return_dict['index'] = index
+
+    if os.path.isfile(os.path.join('video', video_file_name[index])):
+        return json.dumps(return_dict)
+    abort(404)
+
+
+@app.route('/download/<path:url_path>/')
+def download(url_path):
+    if request.method == "GET":
+
+        file_type = url_path.split('/')[0]
+        name = url_path.split('/')[1]
+
+        if os.path.isfile(os.path.join(file_type, name)):
+            return send_from_directory(file_type, name, as_attachment=True)
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
