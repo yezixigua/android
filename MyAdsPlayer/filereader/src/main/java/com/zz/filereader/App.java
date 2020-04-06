da056e63bf1693c96b54913cd0c92ef7c0a9593a
yezixigua <541889983@qq.com>
2020-04-06 19:24:29 +0800

    Revert "test sourcetree"
    This reverts commit e5ba3a0ab5b0a69a8a7a39b9152c9c37e608749e.

--- MyAdsPlayer/filereader/src/main/java/com/zz/filereader/App.java
+++ MyAdsPlayer/filereader/src/main/java/com/zz/filereader/App.java
@@ -22,7 +22,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
 //        试试使用sourcetree
-//        haha
         super.onCreate();
         context = getApplicationContext();
 
